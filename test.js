a5fbcae571dddabcd887e760316d813284fcc5e4
jxck <block.rxckin.beats@gmail.com>
2021-05-13 17:37:04 +0900

    add test.js

---
+++ test.js
@@ -0,0 +1,47 @@
+import { base64decode, decodeToken, extractOT } from "./util.js"
+
+function assert(a, b) {
+    console.log(a, b)
+    console.assert(a === b)
+}
+
+function test_base64decode() {
+    const bin = base64decode("QUJDREVGRw==")
+    assert(new TextDecoder().decode(bin), "ABCDEFG")
+}
+
+function test_decodeToken() {
+    const token = "ArSxvrfZ5EuQwmLLygXLeOO+U8YwgKmylzybnDThaDjzKW/3X98oWOCjtLmNm4Ea6a83sFRlzl9pyYovLr+fsw4AAABkeyJvcmlnaW4iOiJodHRwczovL2xhYnMuanhjay5pbzo0NDMiLCJmZWF0dXJlIjoiVHJ1c3RUb2tlbnMiLCJleHBpcnkiOjE2MDg4NzgyOTcsImlzU3ViZG9tYWluIjp0cnVlfQ=="
+    const { version, signature, length, payload } = decodeToken(token)
+    assert(version, 2)
+    assert(length, 100)
+    assert(signature instanceof Uint8Array, true)
+    assert(signature.length, 64)
+
+    const { origin, feature, expiry, isSubdomain } = payload
+    assert(origin, 'https://labs.jxck.io:443')
+    assert(feature, 'TrustTokens')
+    assert(expiry, 1608878297)
+    assert(isSubdomain, true)
+}
+
+function test_extractOT() {
+    const result = extractOT([
+        { name: 'origin-trial', value: 'token-a' },
+        { name: 'origin-trial', value: 'token-b, token-c' },
+        { name: 'foo', value: 'bar' },
+    ])
+
+    result.forEach(({ type, value }) => {
+        assert(type, 'HTTP Header')
+        assert(['token-a', 'token-b', 'token-c'].includes(value), true)
+    })
+
+    assert(extractOT([]).length, 0)
+}
+
+;[
+    test_base64decode,
+    test_decodeToken,
+    test_extractOT,
+].forEach((test_case) => test_case())
--- util.js
+++ util.js
@@ -3,12 +3,12 @@ export function base64decode(str) {
 }
 
 export function decodeToken(token) {
-    const buf = base64decode(token)
-    const view = new DataView(buf.buffer)
-    const version = view.getUint8()
+    const buf       = base64decode(token)
+    const view      = new DataView(buf.buffer)
+    const version   = view.getUint8()
     const signature = buf.slice(1, 65)
-    const length = view.getUint32(65, false)
-    const payload = JSON.parse((new TextDecoder()).decode(buf.slice(69, 69 + length)))
+    const length    = view.getUint32(65, false)
+    const payload   = JSON.parse((new TextDecoder()).decode(buf.slice(69, 69 + length)))
     return { version, signature, length, payload }
 }
 
