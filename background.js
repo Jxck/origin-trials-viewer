ea338e863cd50c53d002c14568f9fd0a1b39e4a0
jxck <block.rxckin.beats@gmail.com>
2021-05-13 14:27:09 +0900

    fixup background.js

--- background.js
+++ background.js
@@ -6,27 +6,30 @@ const filters = {
 // we need 'var' for global scope
 var headers = {};
 
-chrome.webRequest.onHeadersReceived.addListener(function(details) {
-  headers[details.tabId] = headers[details.tabId] || {};
-  headers[details.tabId][details.frameId] = headers[details.tabId][details.frameId] || {};
-  headers[details.tabId][details.frameId].response = details;
-}, filters, ["responseHeaders"]);
+chrome.webRequest.onHeadersReceived.addListener(function (details) {
+  const { tabId, frameId } = details
+  headers[tabId] = headers[tabId] || {}
+  headers[tabId][frameId] = headers[tabId][frameId] || {}
+  headers[tabId][frameId].response = details
+}, filters, ["responseHeaders"])
 
 // delete persisted headers when a tab is removed
-chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
-	delete headers[tabId];
-});
+chrome.tabs.onRemoved.addListener(function (tabId, removeInfo) {
+  console.log(removeInfo)
+  delete headers[tabId]
+})
 
 // on navigation, check which frames still exist and delete persisted headers
 // for frames which do not exist anymore
-chrome.webNavigation.onCompleted.addListener(function(details) {
-  chrome.webNavigation.getAllFrames({tabId: details.tabId}, function(framesInfo) {
-    const currentFrames = framesInfo.map(frameInfo => frameInfo.frameId);
-    const headersForTab = headers[details.tabId];
+chrome.webNavigation.onCompleted.addListener((details) => {
+  const { tabId } = details
+  chrome.webNavigation.getAllFrames({ tabId }, (framesInfo) => {
+    const frameIds = framesInfo.map(({ frameId }) => frameId)
+    const headersForTab = headers[tabId]
     if (headersForTab) {
-      const asArray = Object.entries(headersForTab);
-      const filteredArray = asArray.filter(([frameId, response]) => currentFrames.includes(Number(frameId)));
-      headers[details.tabId] = Object.fromEntries(filteredArray);
+      const asArray = Object.entries(headersForTab)
+      const filteredArray = asArray.filter(([frameId, response]) => frameIds.includes(Number(frameId)))
+      headers[tabId] = Object.fromEntries(filteredArray)
     }
-  });
-}, filters);
+  })
+}, filters)
